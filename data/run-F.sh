26de56eb4f9df068f7dc808d00b29adb560ed2de
ddeutsch <ddeutsch@jhu.edu>
2013-02-08 16:39:52 -0500

    Added the *-F2.txt files

---
+++ data/run-F.sh
@@ -0,0 +1,37 @@
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes
+
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=Yes
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=No
+
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes PreviousFluRate=Elevated
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes PreviousFluRate=NotElevated
+
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes MaryVaccinated=Yes
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes MaryVaccinated=No
+
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=Yes,PreviousFluRate=Elevated
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=Yes,PreviousFluRate=NotElevated
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=No,PreviousFluRate=Elevated
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=No,PreviousFluRate=NotElevated
+
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=Yes,MaryVaccinated=Yes
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=Yes,MaryVaccinated=No
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=No,MaryVaccinated=Yes
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=No,MaryVaccinated=No
+
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes PreviousFluRate=Elevated,MaryVaccinated=Yes
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes PreviousFluRate=Elevated,MaryVaccinated=No
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes PreviousFluRate=NotElevated,MaryVaccinated=Yes
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes PreviousFluRate=NotElevated,MaryVaccinated=No
+
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=Yes,PreviousFluRate=Elevated,MaryVaccinated=Yes
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=Yes,PreviousFluRate=Elevated,MaryVaccinated=No
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=Yes,PreviousFluRate=NotElevated,MaryVaccinated=Yes
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=Yes,PreviousFluRate=NotElevated,MaryVaccinated=No
+
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=No,PreviousFluRate=Elevated,MaryVaccinated=Yes
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=No,PreviousFluRate=Elevated,MaryVaccinated=No
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=No,PreviousFluRate=NotElevated,MaryVaccinated=Yes
+python bayes-query.py network-F.txt cpd-F.txt MaryGetsFlu=Yes IsFluSeason=No,PreviousFluRate=NotElevated,MaryVaccinated=No
+
+
