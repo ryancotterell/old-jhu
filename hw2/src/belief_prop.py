964c7519298e0fb1f9f14a655b1d111fca4ab2bb
Dan Crankshaw <crankshaw@google.com>
2013-03-08 18:03:11 -0500

    Debugging some stuff.

--- hw2/src/belief_prop.py
+++ hw2/src/belief_prop.py
@@ -4,16 +4,16 @@ import sys
 
 # we store CPD of a factor in a numpy ndarray
 class Factor:
-  def __init__(self):
-    self.rvs = []
   def __init__(self, rvs):
     self.rvs = rvs.sort()
-    self.cpt = _init_cpt(rvs)
+    print rvs
+    print self.rvs
+    self.cpt = self._init_cpt(rvs)
     
   def _init_cpt(self, rvs):
     dims_list = []
     for r in rvs:
-      dims_list.append(len(r.vals))
+      dims_list.append(len(r.values))
     dims = tuple(dims_list)
     return np.zeros(dims, np.float64)
 
@@ -21,14 +21,13 @@ class Factor:
   def __eq__(self, other):
     if not isinstance(other, Factor):
       return False
-
-    return (set(self.rvs) == set(other.rvs)
+    return (set(self.rvs) == set(other.rvs))
 
   def __hash__(self):
     return hash(frozenset(self.rvs))
 
   def update_cpt_entry(entry, value):
-    if (len(entry) != len(self.rvs):
+    if (len(entry) != len(self.rvs)):
       raise Exception('length mismatch') 
     # order of indices is lexicographical order of RV names in factor
     indices = []
@@ -36,10 +35,14 @@ class Factor:
       if rv_name != rv.name:
         raise Exception('mismatch in rvs grep thishkjashdkjh')
       # find the index that corresponds to this setting of the RV
-      indices.add(rv.get_index(entry[rv_name])
-    self.cpt[tuple(indices)]
+      indices.append(rv.get_index(entry[rv_name]))
+    self.cpt[tuple(indices)] = value
+
   def __str__(self):
     return (str(self.rvs) + "\n" + str(self.cpt) + 'num_parents: ' + str(len(self.parents)) + '\n')
+  
+  def __repr__(self):
+    return str(self)
       
       
     
@@ -49,7 +52,7 @@ class Clique:
     print 'constructed Clique'
 
 class SepSet:
-  def __init__(self, Clique c1, Clique c2):
+  def __init__(self, c1, c2):
     self.i = c1
     self.j = c2
   def __eq__(self, other):
@@ -69,23 +72,25 @@ class Node:
     self.children = []
     self.parents = []
 
-  def add_child(child):
+  def add_child(self, child):
     self.children.append(child)
 
-  def add_parent(par):
+  def add_parent(self, par):
     self.parents.append(par)
 
   # maps string setting value onto a numerical index
   def get_index(self, setting):
     return self.values.index(setting)
   def __str__(self):
-    return ('name: ' + self.name + 'values: ' + str(self.values) + '\n')
+    return ('name: ' + self.name + ' values: ' + str(self.values) + ' parents: ' + str(self.parents) + '\n')
+  def __repr__(self):
+    return str(self)
     
 
-def multiply_factors(Factor phi1, Factor phi2):
+def multiply_factors(phi1, phi2):
   return 0
 
-def divide_factors(Factor phi1, Factor phi2):
+def divide_factors(phi1, phi2):
   return 0
 
 
@@ -96,16 +101,19 @@ def parse_network(netfile):
   nodes = {}
   for i in range(num_vars):
     line = net.readline()
+    line = line.strip()
     halves = line.split(' ')
     name = halves[0]
     vals = halves[1].split(',')
     cur_node = Node(name, vals)
     nodes[name] = cur_node
 
-  for line in netfile:
+  for line in net:
+    line = line.strip()
     comps = line.split(' ')
     nodes[comps[0]].add_child(comps[2])
     nodes[comps[2]].add_parent(comps[0])
+  net.close() 
   return nodes
 
 def create_factors(nodes):
@@ -113,8 +121,10 @@ def create_factors(nodes):
   # TODO(crankshaw) might be good to add factors to cliques here as well
   factors = []
   for name in nodes:
-    nodes_in_factor = list(nodes[name].parents)
+    nodes_in_factor = [nodes[n] for n in nodes[name].parents]
+    # nodes_in_factor = list(nodes[name].parents)
     nodes_in_factor.append(nodes[name])
+    # print nodes_in_factor
     factors.append(Factor(nodes_in_factor))
   return factors
 
@@ -125,11 +135,12 @@ def create_factors(nodes):
 def parse_cpd(nodes, factors, cpd_file): 
   cpd = open(cpd_file, 'r')
   for line in cpd:
+    line = line.strip()
     halves = line.split(' ')
-    num = float(halves[2])
+    value = float(halves[2])
     variables = []
     variables.append(halves[0])
-    variables = variables + halves[2].split(',')
+    variables = variables + halves[1].split(',')
     entry = {}
     for var in variables:
       sp = var.split('=')
@@ -137,6 +148,7 @@ def parse_cpd(nodes, factors, cpd_file):
       setting = sp[1]
       entry[name] = setting
     add_entry_to_factor(factors, entry, value)
+  cpd.close()
 
 # entry is a dictionary containg member names as keys and
 # their settings as values.
@@ -179,7 +191,9 @@ def main(netfile, cpdfile):
 
 
 if __name__=='__main__':
-	main(sys.argv[1], sys.argv[2])
+  # print sys.argv
+  main(sys.argv[1], sys.argv[2])
+
 
 
 
