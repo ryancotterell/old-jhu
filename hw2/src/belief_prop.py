c1232e51c2513ce301e0c5a4de6b6dd230ca939e
Dan Crankshaw <crankshaw@google.com>
2013-03-08 17:25:20 -0500

    added code to initialize factors.

--- hw2/src/belief_prop.py
+++ hw2/src/belief_prop.py
@@ -1,44 +1,203 @@
-import numpy
+import numpy as np
 import sys
 
 
+# we store CPD of a factor in a numpy ndarray
 class Factor:
-	def __init__(self):
-		print 'constructed Factor'
+  def __init__(self):
+    self.rvs = []
+  def __init__(self, rvs):
+    self.rvs = rvs.sort()
+    self.cpt = _init_cpt(rvs)
+    
+  def _init_cpt(self, rvs):
+    dims_list = []
+    for r in rvs:
+      dims_list.append(len(r.vals))
+    dims = tuple(dims_list)
+    return np.zeros(dims, np.float64)
 
+  # from http://stackoverflow.com/questions/3076967/python-is-this-an-ok-way-of-overriding-eq-and-hash
+  def __eq__(self, other):
+    if not isinstance(other, Factor):
+      return False
+
+    return (set(self.rvs) == set(other.rvs)
+
+  def __hash__(self):
+    return hash(frozenset(self.rvs))
+
+  def update_cpt_entry(entry, value):
+    if (len(entry) != len(self.rvs):
+      raise Exception('length mismatch') 
+    # order of indices is lexicographical order of RV names in factor
+    indices = []
+    for rv_name, rv in zip(sorted(entry), self.rvs):
+      if rv_name != rv.name:
+        raise Exception('mismatch in rvs grep thishkjashdkjh')
+      # find the index that corresponds to this setting of the RV
+      indices.add(rv.get_index(entry[rv_name])
+    self.cpt[tuple(indices)]
+  def __str__(self):
+    return (str(self.rvs) + "\n" + str(self.cpt) + 'num_parents: ' + str(len(self.parents)) + '\n')
+      
+      
+    
 
 class Clique:
-	def __init__(self):
-		print 'constructed Factor'
+  def __init__(self):
+    print 'constructed Clique'
 
 class SepSet:
-	i = None
-	j = None
-	def __init__(self, Clique c1, Clique c2):
-		i = c1
-		j = c2
-	def __eq__(self, other):
-		if (self.i == other.i and self.j == other.j):
-			return True
-		elif (self.j == other.i and self.i == other.j):
-			return True
-		else:
-			return False
-		
-		
+  def __init__(self, Clique c1, Clique c2):
+    self.i = c1
+    self.j = c2
+  def __eq__(self, other):
+    if (self.i == other.i and self.j == other.j):
+      return True
+    elif (self.j == other.i and self.i == other.j):
+        return True
+    else:
+        return False
 
+class Node:
+  def __init__(self, nm, vals):
+    self.values = vals
+    # sort values so they will be in alphabetical order
+    self.values.sort()
+    self.name = nm
+    self.children = []
+    self.parents = []
+
+  def add_child(child):
+    self.children.append(child)
+
+  def add_parent(par):
+    self.parents.append(par)
+
+  # maps string setting value onto a numerical index
+  def get_index(self, setting):
+    return self.values.index(setting)
+  def __str__(self):
+    return ('name: ' + self.name + 'values: ' + str(self.values) + '\n')
+    
 
 def multiply_factors(Factor phi1, Factor phi2):
-	return 0
+  return 0
 
 def divide_factors(Factor phi1, Factor phi2):
-	return 0
+  return 0
 
 
+def parse_network(netfile):
+  net = open(netfile, 'r')
+  # need file error handling
+  num_vars = int(net.readline())
+  nodes = {}
+  for i in range(num_vars):
+    line = net.readline()
+    halves = line.split(' ')
+    name = halves[0]
+    vals = halves[1].split(',')
+    cur_node = Node(name, vals)
+    nodes[name] = cur_node
 
-def main():
-	print 'hello'
+  for line in netfile:
+    comps = line.split(' ')
+    nodes[comps[0]].add_child(comps[2])
+    nodes[comps[2]].add_parent(comps[0])
+  return nodes
+
+def create_factors(nodes):
+  # now create factors
+  # TODO(crankshaw) might be good to add factors to cliques here as well
+  factors = []
+  for name in nodes:
+    nodes_in_factor = list(nodes[name].parents)
+    nodes_in_factor.append(nodes[name])
+    factors.append(Factor(nodes_in_factor))
+  return factors
+
+  # maybe name factor based on a hash of all nodes in the factor
+
+  # when reading in clique tree ensure that it contains correct factors
+
+def parse_cpd(nodes, factors, cpd_file): 
+  cpd = open(cpd_file, 'r')
+  for line in cpd:
+    halves = line.split(' ')
+    num = float(halves[2])
+    variables = []
+    variables.append(halves[0])
+    variables = variables + halves[2].split(',')
+    entry = {}
+    for var in variables:
+      sp = var.split('=')
+      name = sp[0]
+      setting = sp[1]
+      entry[name] = setting
+    add_entry_to_factor(factors, entry, value)
+
+# entry is a dictionary containg member names as keys and
+# their settings as values.
+# value is the value of the CPT with these settings
+def add_entry_to_factor(factors, entry, value):
+  factor_dummy = Factor(entry.keys())
+  # should find factor with matching random variables
+  # because I overrode __eq__() in Factor
+  fact_index = factors.index(factor_dummy)
+  factor = factors[factor_index]
+  factor.update_cpt_entry(entry, value)
+  
+  
+
+# TODO(crankshaw) update all factors with no parents
+# so that the entries reflect the uniform distribution
+def update_cpds_for_single_node_factors(factors):
+  for factor in factors:
+    if len(factor.rvs) == 1:
+      # this factor has no parents
+      node = factor.rvs[0]
+      if len(node.parents) > 0:
+        raise Exception('factor has one node that has parents')
+      # fill in CPT with uniform distribution
+      factor.cpt = np.ones(len(node.values))/np.float64(len(node.values))
+      
+    
+
+def main(netfile, cpdfile):
+  nodes = parse_network(netfile)
+  factors = create_factors(nodes)
+  parse_cpd(nodes, factors, cpdfile) 
+  update_cpds_for_single_node_factors(factors)
+  for f in factors:
+    print f
+  # I now have all of my factors complete with cpds
+  # time to put them into clique
+
+
 
 
 if __name__=='__main__':
-	main()
+	main(sys.argv[1], sys.argv[2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
