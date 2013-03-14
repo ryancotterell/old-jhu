a481cbd1f05c38bcc89a592a61c265f2be542728
Dan Crankshaw <dscrankshaw@gmail.com>
2013-03-14 08:17:36 -0700

    added some stuff.

--- hw2/src/belief_prop.py
+++ hw2/src/belief_prop.py
@@ -6,106 +6,106 @@ import random
 
 # we store CPD of a factor in a numpy ndarray
 class Factor:
-  def __init__(self, rvs, cpt=None):
-    rvs.sort()
+    def __init__(self, rvs, cpt=None):
+        rvs.sort()
     self.rvs = list(rvs)
     self.rvs.sort()
     if cpt == None:
-      self.cpt = self._init_cpt(rvs)
+        self.cpt = self._init_cpt(rvs)
     else:
-      self.cpt = np.copy(cpt)
-    
+        self.cpt = np.copy(cpt)
+
   def _init_cpt(self, rvs):
-    dims_list = []
+      dims_list = []
     for r in rvs:
-      dims_list.append(len(r.values))
+        dims_list.append(len(r.values))
     dims = tuple(dims_list)
     return np.zeros(dims, np.float64)
 
-  # from http://stackoverflow.com/questions/3076967/python-is-this-an-ok-way-of-overriding-eq-and-hash
+# from http://stackoverflow.com/questions/3076967/python-is-this-an-ok-way-of-overriding-eq-and-hash
   def __eq__(self, other):
-    if not isinstance(other, Factor):
-      return False
+      if not isinstance(other, Factor):
+          return False
     return (set(self.rvs) == set(other.rvs))
 
-  def __hash__(self):
+def __hash__(self):
     return hash(frozenset(self.rvs))
 
-  def update_cpt_entry(self, entry, value):
+def update_cpt_entry(self, entry, value):
     if (len(entry) != len(self.rvs)):
-      raise Exception('length mismatch') 
+        raise Exception('length mismatch') 
     # order of indices is lexicographical order of RV names in factor
     indices = []
     for rv_name, rv in zip(sorted(entry), sorted(self.rvs)):
-      if rv_name != rv.name:
-        print entry
+        if rv_name != rv.name:
+            print entry
         print self.rvs
         raise Exception((rv_name + ' ' +  rv.name))
-      # find the index that corresponds to this setting of the RV
+    # find the index that corresponds to this setting of the RV
       indices.append(rv.get_index(entry[rv_name]))
     self.cpt[tuple(indices)] = value
 
   def __str__(self):
-    return (str(self.rvs) + "\n" + str(self.cpt) + '\n')
-  
+      return (str(self.rvs) + "\n" + str(self.cpt) + '\n')
+
   def __repr__(self):
-    return str(self)
-      
-      
-    
+      return str(self)
+
+
+
 
 class Clique:
-  def __init__(self, factors, nodes):
-    self.factors = factors
+    def __init__(self, factors, nodes):
+        self.factors = factors
     self.nodes = nodes
     self.neighbors = []
     self.belief = None
   def add_neighbor(self, neighbor):
-    self.neighbors.append(neighbor)
+      self.neighbors.append(neighbor)
 
   def collect_message(self, requester, sepsets):
-    nbr_set = set(self.neighbors)
+      nbr_set = set(self.neighbors)
     # BASE CASE
     if set(requester) == nbr_set:
-      self.send_message(requester, sepsets)
+        self.send_message(requester, sepsets)
     # else we wait for all our children to send messages to us, then we send
     # our message back up to the requester (i.e. parent)
-    else:
-      nbr_set.remove(requester)
+else:
+    nbr_set.remove(requester)
       for n in nbr_set:
-        n.collect_message(self)
+          n.collect_message(self)
       self.send_message(requester, sepsets)
-  
+
   def send_message(self, dest, sepsets):
-    common_nodes = set(self.nodes).intersection(set(dest.nodes))
+      common_nodes = set(self.nodes).intersection(set(dest.nodes))
     marginal = marginalize_factor(self.belief, common_nodes)
     node_names = [n.name for n in common_nodes]
     relevant_sepset = sepsets[frozenset(node_names)]
     divided_message = divide_factors(marginal, relevant_sepset.message)
     dest.belief = multiply_factors(dest.belief, divided_message)
     relevant_sepset.message = marginal
-  
+
   def distribute_message(self, sender, sepsets):
-    nbr_set = set(self.neighbors)
+      nbr_set = set(self.neighbors)
     # have the sender send us the message
     sender.send_message(self, sepsets)
     # BASE CASE
     if set(sender) == nbr_set:
-      # done
+        # done
     else:
-      nbr_set.remove(sender)
+        nbr_set.remove(sender)
       for n in nbr_set:
-        n.distribute_message(self, sepsets)
-      
-      
-  
-    
-    
-    
+          n.distribute_message(self, sepsets)
+
+
+
+
+
+
 
 class SepSet:
-  def __init__(self, c1, c2):
-    self.i = c1
+    def __init__(self, c1, c2):
+        self.i = c1
     self.j = c2
     self.nodes = list(set(self.i.nodes).intersection(self.j.nodes))
     self.nodes.sort()
@@ -113,8 +113,8 @@ class SepSet:
     self.message = np.ones(dims)
 
   def __eq__(self, other):
-    if (self.i == other.i and self.j == other.j):
-      return True
+      if (self.i == other.i and self.j == other.j):
+          return True
     elif (self.j == other.i and self.i == other.j):
         return True
     else:
@@ -122,8 +122,8 @@ class SepSet:
 
 @functools.total_ordering
 class Node:
-  def __init__(self, nm, vals):
-    self.values = vals
+    def __init__(self, nm, vals):
+        self.values = vals
     # sort values so they will be in alphabetical order
     self.values.sort()
     self.name = nm
@@ -131,73 +131,73 @@ class Node:
     self.parents = []
 
   def add_child(self, child):
-    self.children.append(child)
+      self.children.append(child)
 
   def add_parent(self, par):
-    self.parents.append(par)
+      self.parents.append(par)
 
   # maps string setting value onto a numerical index
   def get_index(self, setting):
-    return self.values.index(setting)
+      return self.values.index(setting)
   def __str__(self):
-    return ('name: ' + self.name + ' values: ' + str(self.values) + ' parents: ' + str(self.parents) + '\n')
+      return ('name: ' + self.name + ' values: ' + str(self.values) + ' parents: ' + str(self.parents) + '\n')
   def __repr__(self):
-    return str(self)
-  
+      return str(self)
+
   def __eq__(self, other):
-    if not isinstance(other, Node):
-      return False
+      if not isinstance(other, Node):
+          return False
     return self.name == other.name
-  def __lt__(self, other):
+def __lt__(self, other):
     return self.name < other.name
-  def __hash__(self):
+def __hash__(self):
     return hash(self.name)
 
 def multiply_factors(phi1, phi2):
-  rv_union = set(phi1.rvs).union(set(phi2.rvs))
+    rv_union = set(phi1.rvs).union(set(phi2.rvs))
   rv_union_list = list(rv_union)
   rv_union_list.sort()
   dims = []
   for r in rv_union_list:
-    dims.append(len(r.values))
+      dims.append(len(r.values))
   shape = np.zeros(dims)
   product = np.zeros(dims)
-  
+
   it = np.nditer(shape, flags=['multi_index'])
   while not it.finished:
-    phi1_index = []
+      phi1_index = []
     for i, ix in zip(rv_union_list, range(len(rv_union_list))):
-      if i in phi1.rvs:
-        phi1_index.append(it.multi_index[ix])
+        if i in phi1.rvs:
+            phi1_index.append(it.multi_index[ix])
     phi2_index = []
     for i, ix in zip(rv_union_list, range(len(rv_union_list))):
-      if i in phi2.rvs:
-        phi2_index.append(it.multi_index[ix])
+        if i in phi2.rvs:
+            phi2_index.append(it.multi_index[ix])
     phi1_index = tuple(phi1_index)
     phi2_index = tuple(phi2_index)
     product[tuple(it.multi_index)] = phi1.cpt[phi1_index] * phi2.cpt[phi2_index]
     it.iternext()
-  
+
   return Factor(rv_union_list, product)
 
 def marginalize_factor(factor, remaining_rvs):
-  dims = [len(r.values) for r in remaining_rvs]
+    dims = [len(r.values) for r in remaining_rvs]
   shape = np.zeros(dims)
   result = np.zeros(dims)
 
   it = np.nditer(factor.cpt, flags=['multi_index'])
   while not it.finished:
-    marginal_index = []
+      marginal_index = []
     for i, ix in zip(factor.rvs, range(len(factor.rvs))):
-      if i in remaining_rvs:
-        marginal_index.append(it.multi_index[ix])
+        if i in remaining_rvs:
+            marginal_index.append(it.multi_index[ix])
     marginal_index = tuple(marginal_index)
     result[marginal_index] = result[marginal_index] + factor.cpt[tuple(it.multi_index)]
     it.iternext()
   return Factor(factor.rvs, result)
-    
+
 def test_multiply_factors():
-  rv1 = Node('a', [1, 2, 3])
+    rv1 = Node('a', [1, 2, 3])
   rv2 = Node('b', [1, 2])
   rv3 = Node('c', [1, 2])
   phi1 = Factor([rv1, rv2], cpt=None)
@@ -226,39 +226,39 @@ def test_multiply_factors():
   print result1
   print '\n'
   print result2
-  
+
 
 # computes factor quotient of psi / phi
 def divide_factors(psi, phi):
-  psi_set = set(psi.rvs)
+    psi_set = set(psi.rvs)
   phi_set = set(phi.rvs)
   if not phi_set <= psi_set:
-    raise Exception('malformed denominator')
+      raise Exception('malformed denominator')
   dims = []
   for r in psi.rvs:
-    dims.append(len(r.values))
+      dims.append(len(r.values))
   shape = np.zeros(dims)
   result = np.zeros(dims)
-  
+
   it = np.nditer(shape, flags=['multi_index'])
   while not it.finished:
-    phi_index = []
+      phi_index = []
     for i, ix in zip(psi.rvs,  range(len(psi.rvs))):
-      if i in phi.rvs:
-        phi_index.append(it.multi_index[ix])
+        if i in phi.rvs:
+            phi_index.append(it.multi_index[ix])
     phi_index = tuple(phi_index)
     full_ind = tuple(it.multi_index)
     if phi.cpt[phi_index] == 0:
-      if psi.cpt[full_ind] != 0:
-        raise Exception('division by zero')
+        if psi.cpt[full_ind] != 0:
+            raise Exception('division by zero')
     else:
-      result[tuple(it.multi_index)] = psi.cpt[full_ind] / phi.cpt[phi_index]
+        result[tuple(it.multi_index)] = psi.cpt[full_ind] / phi.cpt[phi_index]
     it.iternext()
-  
+
   return Factor(phi.rvs, result)
 
 def test_divide_factors():
-  rv1 = Node('a', [1, 2, 3])
+    rv1 = Node('a', [1, 2, 3])
   rv2 = Node('b', [1, 2])
   psi = Factor([rv1, rv2], cpt=None)
   phi = Factor([rv1], cpt=None)
@@ -280,12 +280,12 @@ def test_divide_factors():
 
 
 def parse_network(netfile):
-  net = open(netfile, 'r')
+    net = open(netfile, 'r')
   # need file error handling
   num_vars = int(net.readline())
   nodes = {}
   for i in range(num_vars):
-    line = net.readline()
+      line = net.readline()
     line = line.strip()
     halves = line.split(' ')
     name = halves[0]
@@ -294,7 +294,7 @@ def parse_network(netfile):
     nodes[name] = cur_node
 
   for line in net:
-    line = line.strip()
+      line = line.strip()
     comps = line.split(' ')
     nodes[comps[0]].add_child(comps[2])
     nodes[comps[2]].add_parent(comps[0])
@@ -302,11 +302,11 @@ def parse_network(netfile):
   return nodes
 
 def create_factors(nodes):
-  # now create factors
+    # now create factors
   # TODO(crankshaw) might be good to add factors to cliques here as well
   factors = {}
   for name in nodes:
-    nodes_in_factor = [nodes[n] for n in nodes[name].parents]
+      nodes_in_factor = [nodes[n] for n in nodes[name].parents]
     node_names = list(nodes[name].parents)
     node_names.append(name)
     # nodes_in_factor = list(nodes[name].parents)
@@ -320,9 +320,9 @@ def create_factors(nodes):
   # when reading in clique tree ensure that it contains correct factors
 
 def parse_cpd(nodes, factors, cpd_file): 
-  cpd = open(cpd_file, 'r')
+    cpd = open(cpd_file, 'r')
   for line in cpd:
-    line = line.strip()
+      line = line.strip()
     # print line
     halves = line.split(' ')
     # print halves
@@ -333,7 +333,7 @@ def parse_cpd(nodes, factors, cpd_file):
     # print variables
     entry = {}
     for var in variables:
-      sp = var.split('=')
+        sp = var.split('=')
       name = sp[0]
       setting = sp[1]
       entry[name] = setting
@@ -344,7 +344,7 @@ def parse_cpd(nodes, factors, cpd_file):
 # their settings as values.
 # value is the value of the CPT with these settings
 def add_entry_to_factor(factors, entry, value):
-  # key is an immutable set of the names of all RVs in the factor
+    # key is an immutable set of the names of all RVs in the factor
   # keyset = set()
   # for name in entry:
   #     keyset.add(name)
@@ -353,48 +353,48 @@ def add_entry_to_factor(factors, entry, value):
   factor.update_cpt_entry(entry, value)
 
 def add_entry_to_factor_test(factor, entry, value):
-  # key is an immutable set of the names of all RVs in the factor
+    # key is an immutable set of the names of all RVs in the factor
   # keyset = set()
   # for name in entry:
   #     keyset.add(name)
   key = frozenset(iter(entry))
   factor = factors[key]
   factor.update_cpt_entry(entry, value)
-  
-  
+
+
 
 # TODO(crankshaw) update all factors with no parents
 # so that the entries reflect the uniform distribution
 def update_cpds_for_single_node_factors(factors):
-  for f, factor in factors.iteritems():
-    if len(factor.rvs) == 1:
-      # this factor has no parents
+    for f, factor in factors.iteritems():
+        if len(factor.rvs) == 1:
+            # this factor has no parents
       node = factor.rvs[0]
       if len(node.parents) > 0:
-        raise Exception('factor has one node that has parents')
+          raise Exception('factor has one node that has parents')
       # fill in CPT with uniform distribution
       factor.cpt = np.ones(len(node.values))/np.float64(len(node.values))
       print factor
 
 def create_cliques(clique_file, factors):
-  with open(clique_file, 'r') as read_cliques:
-    cliques = {}
+    with open(clique_file, 'r') as read_cliques:
+        cliques = {}
     num_cliques = int(read_cliques.readline())
     for i in range(num_cliques):
-      line = read_cliques.readline().strip()
+        line = read_cliques.readline().strip()
       comps = line.split(',')
       current_clique = frozenset(comps)
       factors_in_clique = {}
       for i, f in factors.iteritems():
-        # check if factor is a subset of this clique
+          # check if factor is a subset of this clique
         if i <= current_clique:
-          factors_in_clique[i] = f
+            factors_in_clique[i] = f
       cliques[current_clique] = Clique(factors_in_clique, current_clique)
     sepsets = {}
     for line in read_cliques:
-      parts = line.strip().split(' ')
+        parts = line.strip().split(' ')
       if len(parts) != 3:
-        raise Exception('malformed clique tree file')
+          raise Exception('malformed clique tree file')
       first_clique_set = frozenset(parts[0].split(','))
       second_clique_set = frozenset(parts[2].split(','))
       first_clique = cliques[first_clique_set]
@@ -405,28 +405,28 @@ def create_cliques(clique_file, factors):
       first_clique.add_neighbor(second_clique)
       second_clique.add_neighbor(first_clique)
   return (cliques, sepsets)
-      
+
 def initialize_cliquetree(cliques, sepsets):
-  for cname, c in cliques.iteritems():
-    c.belief = c.factors[0]
+    for cname, c in cliques.iteritems():
+        c.belief = c.factors[0]
     for i in range(1, len(c.factors)):
-      c.belief = multiply_factors(c.belief, c.factors[i])
- 
+        c.belief = multiply_factors(c.belief, c.factors[i])
+
 def calibrate_cliquetree(cliques, sepsets):
-  # pick arbitrary root
+    # pick arbitrary root
   root = random.randint(len(cliques))
   # upward pass
   for n in root.neighbors:
-    n.collect_message(root, sepsets):
-  # downward pass
+      n.collect_message(root, sepsets):
+          # downward pass
   for n in root.neighbors:
-    n.distribute_message(root, sepsets)
-    
-    
-  
+      n.distribute_message(root, sepsets)
+
+
+
 
 def main(netfile, cpdfile, cliquefile):
-  nodes = parse_network(netfile)
+    nodes = parse_network(netfile)
   factors = create_factors(nodes)
   parse_cpd(nodes, factors, cpdfile) 
   update_cpds_for_single_node_factors(factors)
@@ -436,7 +436,7 @@ def main(netfile, cpdfile, cliquefile):
   # TODO could test this by making sure it satisfies the clique tree invariant
   calibrated_cliques = cliques.copy()
   calibrated_sepsets = cliques.copy()
-  
+
 
 ########################################################
 #                         TODO                         
@@ -447,7 +447,7 @@ def main(netfile, cpdfile, cliquefile):
 
 
 if __name__=='__main__':
-  # print sys.argv
+    # print sys.argv
   # main(sys.argv[1], sys.argv[2])
   # test_multiply_factors()
   test_divide_factors()
